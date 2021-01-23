d9d4dd3920702e8e2f0d45f8a8e198d53f3ef67c
medsep <mehdi.sep@gmail.com>
2021-01-24 03:11:10 +1100

    Updates

---
+++ precache-manifest.d6a52cd1fc567f0c97c762305e6bcaa3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "409a3c3a197d48e321f2fa1cead89d24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3619cd9e0f94237013eb",
+    "url": "/static/js/2.19e89138.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.19e89138.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "238f68ea8025950bfafe",
+    "url": "/static/js/main.3fe17f9f.chunk.js"
+  },
+  {
+    "revision": "c04815f3d96ec2751d3c",
+    "url": "/static/js/runtime-main.a1fe4ea2.js"
+  }
+]);
